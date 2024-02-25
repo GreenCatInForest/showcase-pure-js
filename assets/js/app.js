f97ebb1960ba93d53225daa6154e57aac4838b0c
GreenCatInForest <event2016.2017@gmail.com>
2024-02-25 10:54:24 +0000

    check hostname

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 // Function to detect if the site is deployed on GitHub Pages
 
 isGitHubPages = () => {
-  return window.location.hostname === "GreenCatInForest.github.io";
+  return window.location.hostname === "greencatinforest.github.io";
 };
+
+console.log(window.location.hostname);
 
 const headerNameImgURL = isGitHubPages()
   ? "/showcase-pure-js/assets/img/universe-4609408_1920.jpg"
