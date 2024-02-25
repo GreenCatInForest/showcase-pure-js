4850db16f2c0c858df7479052eff203b71b419ec
GreenCatInForest <event2016.2017@gmail.com>
2024-02-25 10:35:31 +0000

    add conditional render of animation if deployed on GitHub Pages

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Function to detect if the site is deployed on GitHub Pages
+
+isGitHubPages = () => {
+  return window.location.hostname === "username.github.io";
+};
+
+const headerNameImgURL = isGitHubPages()
+  ? "/showcase-pure-js/assets/img/universe-4609408_1920.jpg"
+  : "/assets/img/universe-4609408_1920.jpg";
+
+// Set the CSS variable with the background image URL
+document.documentElement.style.setProperty(
+  "--background-image",
+  `url(${headerNameImgURL})`
+);
