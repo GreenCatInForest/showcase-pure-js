c8838e2a2aa2bf0b6aa0b79ec1b2aa3e1dd3153e
GreenCatInForest <event2016.2017@gmail.com>
2024-02-27 13:15:39 +0000

    add pillars count functionality

--- assets/js/challenges/pillars.js
+++ assets/js/challenges/pillars.js
@@ -1,4 +1,17 @@
-const countDistancePillars = (x, a, b) => {};
+const countDistancePillars = (x, a, b) => {
+  if (x > 0 && a >= 10 && a <= 30 && b >= 10 && b <= 50) {
+    if (x === 1) {
+      return 0;
+    } else if (x > 1) {
+      return (x - 1) * (a * 100) + (x * b - 2 * b);
+    } else {
+      return "some other case";
+    }
+  } else
+    return "Number of pillars must be greater than 1, Distance between pillars must be between 10 and 30 meters, Width of the pillar must be between 10 and 50 centimeters";
+};
+
+console.log(countDistancePillars(1, 10, 10));
 
 const pillars = document.createElement("article");
 pillars.classList.add("pillars", "sandboxItem");
@@ -18,8 +31,8 @@ addEventListener("submit", (e) => {
   countDistancePillarsResult
     ? (document.getElementById(
         "countDistancePillarsResult"
-      ).innerHTML += `<ul>Result: 
-          <li>${countDistancePillarsResult}</li> 
+      ).innerHTML += `<ul>Result:
+          <li>${countDistancePillarsResult}</li>
       </ul>`)
     : null;
   sandbox.appendChild(pillars);
@@ -28,15 +41,14 @@ addEventListener("submit", (e) => {
 pillars.innerHTML = `
       <h2>Pillars</h2>
       <h3>Description:</h3>
-      <p>There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same.</p> 
-      <p>Your function accepts three arguments:
-      <ol> 
+      <p>There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same.</p>
+      <p>Your function accepts three arguments: </p>
+      <ol>
       <li>number of pillars (≥ 1);</li>
       <li>distance between pillars (10 - 30 meters);</li>
       <li>width of the pillar (10 - 50 centimeters).</li>
       <ol>
-      </p>
-      <p>Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).</p>  
+      <p>Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).</p>
       <h3>Example:</h3>
       <p>pillars (11, 15, 30) => 15270</p>
       <p>pillars (2, 20, 25) => 2000</p>
@@ -46,7 +58,7 @@ pillars.innerHTML = `
       <label for="pillarsInput">Enter a number of pillars, distance beetween them and width of the pillar</label>
       <input type="number" id="numOfPillarsInput" pattern="[0-9]*" inputmode="numeric" placeholder="How many pillars?" required/>
       <input type="number" id="distanceInput" pattern="[0-9]*" inputmode="numeric" placeholder="Distance between pillars" required/>
-      <input type="number" id="widthOfPillar" pattern="[0-9]*" inputmode="numeric" placeholder="Width of the pillar" required/>
+      <input type="number" id="widthOfPillarInput" pattern="[0-9]*" inputmode="numeric" placeholder="Width of the pillar" required/>
       <button type="submit">Calculate the distance</button>
       </form>
       <div id="countDistancePillarsResult">
